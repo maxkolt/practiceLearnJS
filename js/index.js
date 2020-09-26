1c6f36e2bccb55d9672efa363dbd759ee201275a
Maxim Koltovich <12345kolt@gmail.com>
2020-09-26 12:26:30 +0300

    продолжаю. работа над обьектами

--- js/index.js
+++ js/index.js
@@ -94,3 +94,28 @@ function isEmpty(obj) {
     }
     return true;
 }
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+let sum = 0;
+for (let key in salaries) {
+    sum += salaries[key];
+}
+console.log(sum);
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
+    }
+}
