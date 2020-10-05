a5f09f9130407a71975944c45aecc3bdd513eb34
Maxim Koltovich <12345kolt@gmail.com>
2020-10-05 21:11:09 +0300

    продолжаю.

--- js/index.js
+++ js/index.js
@@ -119,3 +119,25 @@ function multiplyNumeric(obj) {
         }
     }
 }
+
+//задание 10
+let calculator = {
+    sum() {
+        return this.a + this.b;
+    },
+
+    mul() {
+        return this.a * this.b;
+    },
+
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+
+
