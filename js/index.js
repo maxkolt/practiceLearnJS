82bcf05190254bf8b00fa994eab0cbc670810403
Maxim Koltovich <12345kolt@gmail.com>
2020-10-06 21:16:21 +0300

    продолжаю.работа с числами

--- js/index.js
+++ js/index.js
@@ -202,3 +202,9 @@ alert( random(1, 5) );
 alert( random(1, 5) );
 alert( random(1, 5) );
 
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+alert( randomInteger(1, 3) );
