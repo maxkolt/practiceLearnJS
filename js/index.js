4c2595899d1446901acb9a88d93369baf88970c5
Maxim Koltovich <12345kolt@gmail.com>
2020-10-06 21:09:26 +0300

    продолжаю.работа с числами

--- js/index.js
+++ js/index.js
@@ -195,3 +195,10 @@ console.log(`Число: ${readNumber()}`);
 
 
 
+function random(min, max) {
+    return min + Math.random() * (max - min);
+}
+alert( random(1, 5) );
+alert( random(1, 5) );
+alert( random(1, 5) );
+
