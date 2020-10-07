679156329e9333a8d987438f5b04bd37b3e0b028
Maxim Koltovich <12345kolt@gmail.com>
2020-10-07 23:21:33 +0300

    продолжаю.работа с строками

--- js/index.js
+++ js/index.js
@@ -208,3 +208,11 @@ function randomInteger(min, max) {
     return Math.floor(rand);
 }
 alert( randomInteger(1, 3) );
+
+//задание 11
+
+function ucFirst(str) {
+    if (!str) return str;
+    return str[0].toUpperCase() + str.slice(1);
+}
+alert( ucFirst("вася") ); // Вася;
