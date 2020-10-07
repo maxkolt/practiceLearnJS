278b6c646d280b20e5688ebf66c79b5df7bef817
Maxim Koltovich <12345kolt@gmail.com>
2020-10-07 23:33:52 +0300

    продолжаю.работа с строками.Практичечкий ничего не понял про строки

--- js/index.js
+++ js/index.js
@@ -225,3 +225,15 @@ function checkSpam(str) {
 alert( checkSpam('buy ViAgRA now') );
 alert( checkSpam('free xxxxx') );
 alert( checkSpam("innocent rabbit") );
+
+
+function truncate(str, maxlength) {
+    return (str.length > maxlength) ?
+        str.slice(0, maxlength - 1) + '…' : str;
+}
+
+
+function extractCurrencyValue(str) {
+    return +str.slice(1);
+}
+
