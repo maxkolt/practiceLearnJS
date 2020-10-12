e7dc89b75128308892f3abfbd13b5046dac95262
Maxim Koltovich <12345kolt@gmail.com>
2020-10-12 22:45:06 +0300

    изучаю массивы

--- js/index.js
+++ js/index.js
@@ -249,3 +249,20 @@ styles[Math.floor((styles.length - 1) / 2)] = "Классика";
 alert(styles.shift());
 styles.unshift("Рэп", "Регги");
 
+
+function sumInput() {
+    let numbers = [];
+    while (true) {
+        let value = prompt("Введите число", 0);
+        if (value === "" || value === null || !isFinite(value)) break;
+        numbers.push(+value);
+    }
+
+    let sum = 0;
+    for (let number of numbers) {
+        sum += number;
+    }
+    return sum;
+}
+
+alert(sumInput());
