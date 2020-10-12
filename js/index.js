f5dbeb04ec38838342928b5b81bc7dda5907c75f
Maxim Koltovich <12345kolt@gmail.com>
2020-10-12 22:37:43 +0300

    изучаю массивы

--- js/index.js
+++ js/index.js
@@ -181,33 +181,35 @@ alert(accumulator.value);
 //задание 11
 let a = +prompt("Введите первое число", "");
 let b = +prompt("Введите второе число", "");
-alert( a + b );
+alert(a + b);
 
 function readNumber() {
     let num
     do {
         num = prompt("Введите число", 0);
-    } while ( !isFinite(num) );
+    } while (!isFinite(num));
     if (num === null || num === '') return null;
     return +num;
 }
-console.log(`Число: ${readNumber()}`);
 
+console.log(`Число: ${readNumber()}`);
 
 
 function random(min, max) {
     return min + Math.random() * (max - min);
 }
-alert( random(1, 5) );
-alert( random(1, 5) );
-alert( random(1, 5) );
+
+alert(random(1, 5));
+alert(random(1, 5));
+alert(random(1, 5));
 
 
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
 }
-alert( randomInteger(1, 3) );
+
+alert(randomInteger(1, 3));
 
 //задание 11
 
@@ -215,16 +217,18 @@ function ucFirst(str) {
     if (!str) return str;
     return str[0].toUpperCase() + str.slice(1);
 }
-alert( ucFirst("вася") );
+
+alert(ucFirst("вася"));
 
 
 function checkSpam(str) {
     let lowerStr = str.toLowerCase();
     return lowerStr.includes('viagra') || lowerStr.includes('xxx');
 }
-alert( checkSpam('buy ViAgRA now') );
-alert( checkSpam('free xxxxx') );
-alert( checkSpam("innocent rabbit") );
+
+alert(checkSpam('buy ViAgRA now'));
+alert(checkSpam('free xxxxx'));
+alert(checkSpam("innocent rabbit"));
 
 
 function truncate(str, maxlength) {
@@ -236,4 +240,12 @@ function truncate(str, maxlength) {
 function extractCurrencyValue(str) {
     return +str.slice(1);
 }
+
+//задание 12
+
+let styles = ["Джаз", "Блюз"];
+styles.push("Рок-н-ролл");
+styles[Math.floor((styles.length - 1) / 2)] = "Классика";
+alert(styles.shift());
+styles.unshift("Рэп", "Регги");
 
