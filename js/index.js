7cfa0cae7a662ce5a0f23300e3fa6f2c75a2c409
Maxim Koltovich <12345kolt@gmail.com>
2020-10-24 23:06:59 +0300

    изучаю методы массивов

--- js/index.js
+++ js/index.js
@@ -308,3 +308,13 @@ alert(arr);
 let arr = [5, 2, 1, -10, 8];
 arr.sort((a, b) => b - a);
 alert(arr);
+
+
+function copySorted(arr) {
+    return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+alert(sorted);
+alert(arr);
