8bf60cf9815af3fe7b4abc4dd56415855f199f63
Maxim Koltovich <12345kolt@gmail.com>
2020-10-24 22:23:33 +0300

    изучаю методы массивов

--- js/index.js
+++ js/index.js
@@ -267,6 +267,7 @@ function sumInput() {
 
 alert(sumInput());
 
+
 //задание 13
 
 function camelize(str) {
@@ -286,5 +287,24 @@ function filterRange(arr, a, b) {
 let arr = [5, 3, 8, 1];
 let filtered = filterRange(arr, 1, 4);
 alert(filtered);
+alert(arr);
 
+
+function filterRangeInPlace(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+        let val = arr[i];
+        if (val < a || val > b) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4);
+alert(arr);
+
+
+let arr = [5, 2, 1, -10, 8];
+arr.sort((a, b) => b - a);
 alert(arr);
