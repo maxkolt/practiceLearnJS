96934c9e017fb2d15fd04325a45df7caa2c7719b
Maxim Koltovich <12345kolt@gmail.com>
2020-10-24 22:20:33 +0300

    изучаю методы массивов

--- js/index.js
+++ js/index.js
@@ -266,3 +266,25 @@ function sumInput() {
 }
 
 alert(sumInput());
+
+//задание 13
+
+function camelize(str) {
+    return str
+        .split('-')
+        .map(
+            (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+        )
+        .join('');
+}
+
+
+function filterRange(arr, a, b) {
+    return arr.filter(item => (a <= item && item <= b));
+}
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+alert(filtered);
+
+alert(arr);
