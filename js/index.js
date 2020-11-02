412012b19befa3b5e44c8180f6432706c90d9536
Maxim Koltovich <12345kolt@gmail.com>
2020-11-02 22:07:49 +0300

    изучаю методы массивов

--- js/index.js
+++ js/index.js
@@ -369,3 +369,21 @@ let usersMapped = users.map(user => ({
 }));
 
 alert( usersMapped[0].fullName );
+
+
+function sortByAge(arr) {
+    arr.sort((a, b) => a.age > b.age ? 1 : -1);
+}
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [ vasya, petya, masha ];
+
+sortByAge(arr);
+
+
+alert(arr[0].name);
+alert(arr[1].name);
+alert(arr[2].name);
