c58f66f5f4da0149fbbda62003425a2833f7b386
Maxim Koltovich <12345kolt@gmail.com>
2020-11-02 22:06:12 +0300

    изучаю методы массивов

--- js/index.js
+++ js/index.js
@@ -345,3 +345,27 @@ function Calculator() {
         this.methods[name] = func;
     };
 }
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map(item => item.name);
+
+alert( names );
+
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+let usersMapped = users.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id
+}));
+
+alert( usersMapped[0].fullName );
