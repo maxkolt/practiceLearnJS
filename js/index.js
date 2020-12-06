a43177295bbac5d243c865b5527e07ffbb9f2afd
maxkolt <12345kolt@gmail.com>
2020-12-06 12:22:11 +0300

    продолжаю изучать

--- js/index.js
+++ js/index.js
@@ -387,3 +387,17 @@ sortByAge(arr);
 alert(arr[0].name);
 alert(arr[1].name);
 alert(arr[2].name);
+
+
+function topSalary(salaries) {
+    let max = 0;
+    let maxName = null;
+
+    for(const [name, salary] of Object.entries(salaries)) {
+        if (max < salary) {
+            max = salary;
+            maxName = name;
+        }
+    }
+    return maxName;
+}
