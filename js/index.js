da145b01a4da415607e6c2b13dea2a962fb1cf10
maxkolt <12345kolt@gmail.com>
2020-12-06 18:17:55 +0300

    продолжаю изучать

--- js/index.js
+++ js/index.js
@@ -401,3 +401,30 @@ function topSalary(salaries) {
     }
     return maxName;
 }
+//
+
+let user = {
+    name: "Василий Иванович",
+    age: 35
+};
+let user2 = JSON.parse(JSON.stringify(user));
+
+//
+
+let room = {
+    number: 23
+};
+
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+    place: room
+};
+
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+    alert(`${key}: ${value}`);
+    return (key != "" && value == meetup) ? undefined : value;
+}));
