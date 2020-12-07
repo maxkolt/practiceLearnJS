2135eb6c50fbd3b6f10cbd0f6e6f31cf98a4767b
maxkolt <12345kolt@gmail.com>
2020-12-07 21:38:23 +0300

    продолжаю изучать, обьект функции

--- js/index.js
+++ js/index.js
@@ -501,3 +501,41 @@ let users = [
 ];
 users.sort(byField('name'));
 users.sort(byField('age'));
+
+//
+
+function makeCounter() {
+    let count = 0;
+
+    function counter() {
+        return count++;
+    }
+
+    counter.set = value => count = value;
+
+    counter.decrease = () => count--;
+
+    return counter;
+}
+
+
+function sum(a) {
+
+    let currentSum = a;
+
+    function f(b) {
+        currentSum += b;
+        return f;
+    }
+
+    f.toString = function() {
+        return currentSum;
+    };
+
+    return f;
+}
+
+alert( sum(1)(2) );
+alert( sum(5)(-1)(2) );
+alert( sum(6)(-1)(-2)(-3) );
+alert( sum(0)(1)(2)(3)(4)(5) );
