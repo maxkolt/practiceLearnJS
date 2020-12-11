e851f339d0feae6e60b602c7a707a7995df2c9f5
maxkolt <12345kolt@gmail.com>
2020-12-11 22:07:00 +0300

    протатип

--- js/index.js
+++ js/index.js
@@ -594,3 +594,29 @@ function delay(f, ms) {
 let f1000 = delay(alert, 1000);
 
 f1000("test");
+
+//
+
+let head = {
+    glasses: 1
+};
+
+let table = {
+    pen: 3,
+    __proto__: head
+};
+
+let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__: table
+};
+
+let pockets = {
+    money: 2000,
+    __proto__: bed
+};
+
+alert( pockets.pen );
+alert( bed.glasses );
+alert( table.money );
