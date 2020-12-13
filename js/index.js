c1e6b6f9bf2d0331ae9aa8ce66aa9fc525ba4345
maxkolt <12345kolt@gmail.com>
2020-12-13 16:42:36 +0300

    Методы прототипов, объекты без свойства __proto__

--- js/index.js
+++ js/index.js
@@ -658,3 +658,19 @@ function f(a, b) {
 }
 
 f.defer(1000)(1, 2);
+
+//
+
+function Rabbit(name) {
+    this.name = name;
+}
+Rabbit.prototype.sayHi = function() {
+    alert( this.name );
+}
+
+let rabbit = new Rabbit("Rabbit");
+
+rabbit.sayHi();
+Rabbit.prototype.sayHi();
+Object.getPrototypeOf(rabbit).sayHi();
+rabbit.__proto__.sayHi();
